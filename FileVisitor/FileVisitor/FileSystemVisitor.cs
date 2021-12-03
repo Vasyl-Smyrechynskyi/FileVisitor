3b7122a1eabe9e171de8e878c289b1613355fd15
Smyrechynskyi <41690469+Smyrechynskyi@users.noreply.github.com>
2021-12-03 18:33:27 +0200

    Resolve the task

---
+++ FileVisitor/FileVisitor/FileSystemVisitor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Collections;
+
+namespace FileVisitor
+{
+    public class FileSystemVisitor : IEnumerable<string>
+    {
+        private readonly string _startPath;
+        private readonly Func<string, bool> _searchPredicate;
+        private bool _stopSearch;
+        public event EventHandler Start;
+        public event EventHandler Finish;
+        public event EventHandler<FoundItemEventArgs> FileFound;
+        public event EventHandler<FoundItemEventArgs> DirectoryFound;
+        public event EventHandler<FoundItemEventArgs> FilteredFileFound;
+        public event EventHandler<FoundItemEventArgs> FilteredDirectoryFound;
+
+        public FileSystemVisitor(string startPath, Func<string, bool> searchPredicate)
+        {
+            _startPath = startPath;
+            _searchPredicate = searchPredicate;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            var enumerator = FindFilesAndDirectories();
+            while (enumerator.MoveNext() && !_stopSearch)
+            {
+                yield return enumerator.Current;
+            }
+
+            Finish?.Invoke(this, null);
+        }
+
+        private IEnumerator<string> FindFilesAndDirectories()
+        {
+            Start?.Invoke(this, null);
+
+            return Directory.GetFileSystemEntries(_startPath, "*.*", SearchOption.AllDirectories)
+                .Where((name) =>
+                {
+                    if (Directory.Exists(name))
+                    {
+                        return CheckIfEligible(name, true);
+                    }
+
+                    return CheckIfEligible(name);
+                })
+                .GetEnumerator();
+        }
+
+        private bool CheckIfEligible(string name, bool isDirectory = false)
+        {
+            if (_searchPredicate != null && !(_searchPredicate?.Invoke(name) == true))
+                return false;
+
+            var args = new FoundItemEventArgs();
+
+            if (isDirectory && _searchPredicate is null)
+            {
+                DirectoryFound?.Invoke(this, args);
+            }
+            else if (_searchPredicate is null)
+            {
+                FileFound?.Invoke(this, args);
+            }
+
+            if (isDirectory && _searchPredicate != null)
+            {
+                FilteredDirectoryFound?.Invoke(this, args);
+            }
+            else if (_searchPredicate != null)
+            {
+                FilteredFileFound?.Invoke(this, args);
+            }
+
+            _stopSearch = args.StopSearch;
+
+            if (!string.IsNullOrWhiteSpace(args.Exclude) && name.Contains(args.Exclude))
+                return false;
+
+            return true;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return FindFilesAndDirectories();
+        }
+    }
+
+    public class FoundItemEventArgs : EventArgs
+    {
+        public bool StopSearch { get; set; }
+
+        public string Exclude { get; set; }
+    }
+}
---
+++ FileVisitor/FileVisitor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FileVisitor
+{
+    class Program
+    {
+        private static readonly IList<string> _names = new List<string>();
+        private static string _excludeFilterString;
+        private static bool _stopSearch = false;
+        private static FileSystemVisitor _systemVisitor;
+
+        static void Main(string[] _)
+        {
+            Console.WriteLine("Please enter path to the start directory");
+            var path = Console.ReadLine();
+
+            Console.WriteLine("Please enter filter string");
+            var filterString = Console.ReadLine();
+
+            Console.WriteLine("Please enter filter string to exclude files and directories or press 'Enter'");
+            _excludeFilterString = Console.ReadLine();
+
+            _systemVisitor = new FileSystemVisitor(
+                path,
+                string.IsNullOrWhiteSpace(filterString) ? null : name => name.Contains(filterString));
+            _systemVisitor.Start += (_, _) => Console.WriteLine("Started");
+            _systemVisitor.Finish += FinishHandler;
+            _systemVisitor.FileFound += FoundItemEventHandler;
+            _systemVisitor.DirectoryFound += FoundItemEventHandler;
+            _systemVisitor.FilteredDirectoryFound += FoundItemEventHandler;
+            _systemVisitor.FilteredFileFound += FoundItemEventHandler;
+
+            Task.Run(() =>
+            {
+                foreach (var name in _systemVisitor)
+                {
+                    _names.Add(name);
+                }
+            });
+
+            Console.WriteLine("Press any key to stop the search");
+            Console.ReadKey();
+
+            _stopSearch = true;
+
+            Console.ReadLine();
+        }
+
+        private static void FinishHandler(object sender, EventArgs e)
+        {
+            Console.WriteLine("Finished");
+            foreach (var name in _names)
+            {
+                Console.WriteLine($"Item name - {name}");
+            }
+        }
+
+        private static void FoundItemEventHandler(object obj, FoundItemEventArgs e)
+        {
+            e.Exclude = _excludeFilterString;
+            e.StopSearch = _stopSearch;
+        }
+    }
+}
